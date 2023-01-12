dbd9827bfe0c978f640673b0a6fdc14c13e78c86
Belen Arbizu jimenez <barbizu-@c2r7s3.42malaga.com>
2023-01-12 12:53:57 +0100

    subida1

--- include/minishell.h
+++ include/minishell.h
@@ -1,19 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: gromero- <gromero-@student.42malaga.com>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/12 11:14:53 by gromero-          #+#    #+#             */
-/*   Updated: 2023/01/12 11:18:20 by gromero-         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "../libft/libft.h"
+#include "../libft/libft.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
+#define BEGIN "\001\033[m\002"
+#define CLOSE "\001\033[0m\002"
 
 #endif
--- source/minishell.c
+++ source/minishell.c
@@ -9,10 +9,22 @@
 /*   Updated: 2023/01/12 11:05:59 by gromero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
+#include "../include/minishell.h"
 
 int	main()
 {
-	write (1, "pepe", 4);
-	return (0);
+	while (1)
+    {
+        char    *str = readline(BEGIN "My Shell $" CLOSE);
+        if (str && *str)
+            add_history(str);
+        if (!ft_strncmp(str, "pwd", sizeof(str)))
+        {
+            char    *pwd = malloc(sizeof(char) * 256);
+            pwd = getcwd(pwd, sizeof(pwd));
+            printf("%s\n", pwd);
+            rl_on_new_line();
+        }
+        free(str);
+    }
 }
